db25dd95d129e16794958d507a42ddf41a722536
kathryndunphy <41403091+kathryndunphy@users.noreply.github.com>
2019-01-26 19:32:31 -0600

    Revert "Authentication works"

--- src/App.js
+++ src/App.js
@@ -2,39 +2,14 @@ import React, { Component } from "react";
 
 import Layout from "./components/Layout/Layout";
 import CatadorRoller from "./containers/CatadorRoller/CatadorRoller";
-import Main from "./components/Main";
-import Secret from "./components/Secret";
-import NotFound from "./components/NotFound";
-import Callback from "./components/Callback";
 
 class App extends Component {
   render() {
-
-    let mainComponent = "";
-    switch(this.props.location) {
-      case "":
-        mainComponent = <Main {...this.props} />
-        break;
-      case "secret":
-        mainComponent = this.props.auth.isAuthenticated() ? <Secret {...this.props} /> : <NotFound />;
-        break;
-      case "callback":
-        mainComponent = <Callback />
-        break;
-      default:
-        mainComponent = <NotFound />  
-    }
-
     return (
       <div>
-        <div>
-          {mainComponent}
-        </div>
-        <div>
-          <Layout>
-            <CatadorRoller />
-          </Layout>
-        </div>
+        <Layout>
+          <CatadorRoller />
+        </Layout>
       </div>
     );
   }
